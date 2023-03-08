ab19e2a60e8490a297be40c5ff8b0a606b7fcc29
Ricardo Hendrichs <71273811+Dohruba@users.noreply.github.com>
2023-03-08 08:36:20 +0100

    Positioning updates

---
+++ AReAS2/Assets/Scripts/Manager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public enum Language
+{
+    english = 0,
+    malay = 1
+}
+public class Manager : MonoBehaviour
+{
+    private static Language chosenLanguage = Language.english;
+
+    public static Language ChosenLanguage { get => chosenLanguage; set => chosenLanguage = value; }
+    
+
+
+    private void Start()
+    {
+        StopAllAuidoAndVideo();
+    }
+    public void SetLanguage(Language language)
+    {
+        ChosenLanguage = language;
+    }
+    
+    public void StopAllAuidoAndVideo()
+    {
+        StopAllAudio();
+        StopAllVideo();
+    }
+
+    private void StopAllAudio()
+    {
+        GameObject[] audioSources = GameObject.FindGameObjectsWithTag("VoiceOver");
+
+        foreach(GameObject gameObject in audioSources)
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+    }
+    private void StopAllVideo()
+    {
+        GameObject[] videoSources = GameObject.FindGameObjectsWithTag("VideoPanel");
+
+        foreach(GameObject gameObject in videoSources)
+        {
+            gameObject.GetComponent<VideoPlayer>().Stop();
+        }
+
+    }
+
+    public void OnTapUpdateLanguage(int i)
+    {
+        StopAllAudio();
+        ChosenLanguage = (Language)i;
+        GameObject[] activePages = GameObject.FindGameObjectsWithTag("Page");
+        bool isOn;
+        foreach(GameObject activePage in activePages)
+        {
+            isOn = activePage.GetComponentInChildren<PageController>().GetIsOn();
+            if (activePage != null && isOn)
+            {
+                activePage.GetComponentInChildren<PageController>().SetLanguage();
+            }
+
+        }
+        Debug.Log("Update language: " + ChosenLanguage.ToString());
+    }
+}
